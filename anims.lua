0640468c0b18d30c74f07589f94dd3928b304c15
AzureEpic <gbaste2011@yahoo.com>
2025-07-03 17:59:39 -0400

    Update anims.lua

--- anims.lua
+++ anims.lua
@@ -1,29 +1,25 @@
 local anims = {
-["Party (R6) - 33796059"];
-["Griffin Fly Thing (R6) - 215384594";
-  ["Spinning Arms (R6) - 259438880"];
- ["Jumping Jacks (R6) - 429681631"];
-  ["Random Arm Movement (R6) - 27432691"];
-  ["Dino Feet Walk (R6) - 204328711"];
-  ["Laying On Floor (R6) - 282574440"];
-  ["Back and forth dance (R6) - 429703734"];
-  ["Head Throw (R6) - 35154961"];
-  ["Floating Head (R6) - 121572214"];
-  ["Crouch (R6) - 182724289"];
-  ["Telamonster Jump (R6) - 184574340"];
-  ["Lay On Back R6 - 181525546"];
-  ["Spin (R6) - 188632011"];
-  ["Float Sit (R6) - 179224234"];
-  ["Party Dance 2 (R6) - 45834924"];
-  ["Punch (R6) - 204062532"];
-  ["Barrage (R6) - 126753849"];
-  ["Outwards Punch (R6) - 218504594"];
-  ["Barrel Roll (R6) - 136801964"];
-  ["Sad Sit (R6) - 180612465"];
-  
-  
-
-
+    "Party (R6) - 33796059",
+    "Griffin Fly Thing (R6) - 215384594",
+    "Spinning Arms (R6) - 259438880",
+    "Jumping Jacks (R6) - 429681631",
+    "Random Arm Movement (R6) - 27432691",
+    "Dino Feet Walk (R6) - 204328711",
+    "Laying On Floor (R6) - 282574440",
+    "Back and forth dance (R6) - 429703734",
+    "Head Throw (R6) - 35154961",
+    "Floating Head (R6) - 121572214",
+    "Crouch (R6) - 182724289",
+    "Telamonster Jump (R6) - 184574340",
+    "Lay On Back R6 - 181525546",
+    "Spin (R6) - 188632011",
+    "Float Sit (R6) - 179224234",
+    "Party Dance 2 (R6) - 45834924",
+    "Punch (R6) - 204062532",
+    "Barrage (R6) - 126753849",
+    "Outwards Punch (R6) - 218504594",
+    "Barrel Roll (R6) - 136801964",
+    "Sad Sit (R6) - 180612465",
 }
 
 return anims
