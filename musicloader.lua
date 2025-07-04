2a8b9eeb82884b4494d33769f733742dfb43743d
AzureEpic <gbaste2011@yahoo.com>
2025-07-04 16:17:09 -0400

    Update musicloader.lua

--- musicloader.lua
+++ musicloader.lua
@@ -1,3 +1,10 @@
+--[[ example usage:
+local musicLoader = loadstring(game:HttpGet("https://raw.githubusercontent.com/AzureEpic/noo/refs/heads/main/musicloader.lua"))()
+musicLoader("opm.mp3", "https://raw.githubusercontent.com/AzureEpic/noo/main/sounds/opm.mp3")
+
+
+
+]]
 return function(Name,URL)
 writefile(Name, game:HttpGet(URL)) 
   task.wait()
