1668ed86399974a8a651b50ccb146503448974fd
AzureEpic <gbaste2011@yahoo.com>
2025-07-04 16:32:32 -0400

    Update musicloader.lua

--- musicloader.lua
+++ musicloader.lua
@@ -1,18 +1,28 @@
 --[[ example usage:
 local musicLoader = loadstring(game:HttpGet("https://raw.githubusercontent.com/AzureEpic/noo/refs/heads/main/musicloader.lua"))()
-musicLoader("opm.mp3", "https://raw.githubusercontent.com/AzureEpic/noo/main/sounds/opm.mp3")
-
-
+musicLoader("opm.mp3", "https://raw.githubusercontent.com/AzureEpic/noo/main/sounds/opm.mp3", true, 2, workspace)
 
 ]]
-return function(Name,URL)
-writefile(Name, game:HttpGet(URL)) 
-  task.wait()
-  sound = Instance.new("Sound")
-		sound.SoundId = getcustomasset(Name)
-		sound.Volume = 2
-		sound.Looped = true
-		sound.Parent = game.Players.LocalPlayer.Character:WaitForChild("HumanoidRootPart")
-		sound:Play()
- 
+
+
+return function(Name, URL, looped, volume, parent)
+   
+    writefile(Name, game:HttpGet(URL))
+    task.wait() 
+    local asset = getcustomasset(Name)
+
+    
+    local sound = parent:FindFirstChild("BGMSoundCustom")
+    if not sound then
+        sound = Instance.new("Sound")
+        sound.Name = "BGMSoundCustom"
+        sound.Parent = parent
+    end
+
+   
+    sound.SoundId = asset
+    sound.Looped = looped or false
+    sound.Volume = volume or 1
+
+    sound:Play()
 end
