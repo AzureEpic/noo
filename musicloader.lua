85e0dc5454530a1112998f391087893c3b4eb71c
AzureEpic <gbaste2011@yahoo.com>
2025-07-04 16:16:21 -0400

    Update musicloader.lua

--- musicloader.lua
+++ musicloader.lua
@@ -1,5 +1,11 @@
 return function(Name,URL)
 writefile(Name, game:HttpGet(URL)) 
   task.wait()
-  getcustomasset(Name)
+  sound = Instance.new("Sound")
+		sound.SoundId = getcustomasset(Name)
+		sound.Volume = 2
+		sound.Looped = true
+		sound.Parent = game.Players.LocalPlayer.Character:WaitForChild("HumanoidRootPart")
+		sound:Play()
+ 
 end
