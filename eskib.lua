9f2f46faf623cba93e311bd3dbce6b818b9c5c49
AzureEpic <gbaste2011@yahoo.com>
2025-07-07 13:23:14 -0400

    Update eskib.lua

--- eskib.lua
+++ eskib.lua
@@ -383,3 +383,49 @@ tool.Unequipped:Connect(function()
 	end
 	print("gadfsdfsfs and music stopped")
 end)
+
+
+
+
+local tool = Instance.new("Tool")
+tool.Name = "Car test"
+tool.RequiresHandle = false
+tool.Parent = backpack
+
+writefile("p.mp3", game:HttpGet("https://github.com/AzureEpic/noo/blob/main/sounds/opm.mp3"))
+-- Load Animator if not already loaded
+if not getgenv()["Animator"] then
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/xhayper/Animator/main/Source/Main.lua"))()
+end
+
+local Anim = nil
+local sound = nil
+
+tool.Equipped:Connect(function()
+	local character = player.Character
+	if character then
+		Anim = Animator.new(character, 128039167552632)
+		Anim:Play()
+		Anim.Stopped:Connect(function()
+			Anim:Play()
+		end)
+		sound = Instance.new("Sound")
+		sound.SoundId = getcustomasset("p.mp3")
+		sound.Volume = 2
+		sound.Looped = true
+		sound.Parent = game.Players.LocalPlayer.Character:WaitForChild("HumanoidRootPart")
+		sound:Play()
+	end
+end)
+
+tool.Unequipped:Connect(function()
+	if Anim then
+		Anim:Stop()
+		Anim:Destroy()
+	end
+	if sound then
+		sound:Stop()
+		sound:Destroy()
+	end
+	print("gadfsdfsfs and music stopped")
+end)
