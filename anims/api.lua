b90aa002d8044140fc414546baf4313c1dcd2c3a
AzureEpic <gbaste2011@yahoo.com>
2025-07-08 11:32:14 -0400

    Update api.lua

--- anims/api.lua
+++ anims/api.lua
@@ -1,4 +1,4 @@
-local pathToGithub = "https://raw.githubusercontent.com/AzureEpic/noo/refs/heads/main/anims/"
+local pathToGithub = "https://raw.githubusercontent.com/xhayper/Animator/main/Source/"
 
 local sub = string.sub
 
@@ -28,35 +28,76 @@ end
 
 
 
-getgenv().httpRequireCache = getgenv().httpRequireCache or {}
 
+
+-- 🛠️ Hotfix for Signal.lua (patches missing :Fire method)
+getgenv().httpRequireCache = getgenv().httpRequireCache or {}
 getgenv().httpRequireCache[pathToGithub .. "Nevermore/Signal.lua"] = (function()
+	local HttpService = game:GetService("HttpService")
+
 	local Signal = {}
 	Signal.__index = Signal
+	Signal.ClassName = "Signal"
 
 	function Signal.new()
 		local self = setmetatable({}, Signal)
-		self._bindable = Instance.new("BindableEvent")
+		self._bindableEvent = Instance.new("BindableEvent")
+		self._argMap = {}
+		self._source = ""
+
+		self._bindableEvent.Event:Connect(function(key)
+			self._argMap[key] = nil
+			if (not self._bindableEvent) and (not next(self._argMap)) then
+				self._argMap = nil
+			end
+		end)
+
 		return self
 	end
 
 	function Signal:Fire(...)
-		self._bindable:Fire(...)
+		if not self._bindableEvent then
+			warn(("Signal is already destroyed. %s"):format(self._source))
+			return
+		end
+
+		local args = table.pack(...)
+		local key = HttpService:GenerateGUID(false)
+		self._argMap[key] = args
+		self._bindableEvent:Fire(key)
 	end
 
-	function Signal:Connect(callback)
-		return self._bindable.Event:Connect(callback)
+	function Signal:Connect(handler)
+		if typeof(handler) ~= "function" then
+			error(("connect(%s)"):format(typeof(handler)), 2)
+		end
+
+		return self._bindableEvent.Event:Connect(function(key)
+			local args = self._argMap[key]
+			if args then
+				handler(table.unpack(args, 1, args.n))
+			else
+				error("Missing arg data, probably due to reentrance.")
+			end
+		end)
 	end
 
 	function Signal:Wait()
-		return self._bindable.Event:Wait()
+		local key = self._bindableEvent.Event:Wait()
+		local args = self._argMap[key]
+		if args then
+			return table.unpack(args, 1, args.n)
+		else
+			error("Missing arg data, probably due to reentrance.")
+		end
 	end
 
 	function Signal:Destroy()
-		if self._bindable then
-			self._bindable:Destroy()
-			self._bindable = nil
+		if self._bindableEvent then
+			self._bindableEvent:Destroy()
+			self._bindableEvent = nil
 		end
+		setmetatable(self, nil)
 	end
 
 	return Signal
